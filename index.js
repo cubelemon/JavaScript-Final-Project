d08293c3f4088c9509ef82c48a21f785c8fddf0a
Natasha Png <natashapng.png@gmail.com>
2024-07-09 17:14:35 +0800

    movie page done

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 /* http://www.omdbapi.com/?i=tt3896198&apikey=e2cbd42e */
 const movieListEl = document.querySelector('.movies__wrapper');
 
+
+
+function showMovieInfo(imdbID) {
+    localStorage.setItem("imdbID", imdbID);
+    window.location.href = `/movie.html`;
+}
+
+function movieHtml(movie) {
+    return `<div class = "movie">
+    <div class="movie__img--wrapper">
+        <img src="${movie.Poster}" alt="" class="movie__img">
+        <a class="movie__content" onclick="showMovieInfo('${movie.imdbID}')">
+            <h1>${movie.Title}</h1>
+            <h1>${movie.Year}</h1>
+        </a>
+    </div>
+</div>`
+}
+
+
 async function onSearchChange(event){
-    const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e2cbd42e`).json()
-    console.log(movies)
+    const searchTerm = event.target.value;
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=e2cbd42e&s=${searchTerm}`)
+    const moviesData = await movies.json();
+    console.log(moviesData)
+    movieListEl.innerHTML = moviesData.Search.map(movie => movieHtml(movie)).join('');
 }
 
-async function test() {
-    console.log(await(await fetch('http://www.omdbapi.com/?apikey=e2cbd42e&s')).json())
-}
 
-test()
+
---
+++ movie.js
@@ -0,0 +1,31 @@
+const id = localStorage.getItem("imdbID")
+const movieDeetsEl = document.querySelector('.movie__deets--wrapper')
+
+async function movieInfo() {
+    const moviesInfo = await fetch(`https://www.omdbapi.com/?apikey=e2cbd42e&i=${localStorage.getItem("imdbID")}`)
+    let moviesInfoData = await moviesInfo.json()
+    if (!Array.isArray(moviesInfoData)) {
+        moviesInfoData = [moviesInfoData];
+    }
+
+    movieDeetsEl.innerHTML = moviesInfoData.map((movie) => movieInfoHTML(movie))
+    console.log(moviesInfoData)
+}
+
+movieInfo()
+
+function movieInfoHTML (movie) {
+    return `  <figure class="movie__poster--wrapper">
+                <img src="${movie.Poster}" alt="" />
+              </figure>
+          <div class="movie__info__wrapper">
+            <h1 class="movie__title">${movie.Title}</h1>
+            <h3 class="detail-list"><span class="text-purple">Release Date:</span>${movie.Released}</h3>
+            <h3 class="detail-list"><span class="text-purple">Genre:</span>${movie.Genre}</h3>
+            <h3 class="detail-list"><span class="text-purple">Language:</span>${movie.Language}</h3>
+            <h3 class="detail-list"><span class="text-purple">Actors</span>${movie.Actors}</h3>
+            <h3 class="detail-list"><span class="text-purple">Director:</span>${movie.Director}</h3>
+            <h3 class="detail-list"><span class="text-purple">Writer:</span>${movie.Writer}</h3>
+            <h3 class="detail-list"><span class="text-purple">Plot:</span>${movie.Plot}</h3>
+          </div>`
+}
